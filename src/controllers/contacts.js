8d4748c196570981d1acea0ded0c67e55c07934b
Falconoff <sokolovserega1982@gmail.com>
2025-01-21 23:36:25 +0200

    add ctrlWrapper

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,20 +1,20 @@
 import * as contactServices from '../services/contacts.js';
 
 export const getContactsController = async (req, res) => {
-  try {
-    const contacts = await contactServices.getContacts();
+  // try {
+  const contacts = await contactServices.getContacts();
 
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: 500,
-      message: error.message,
-    });
-  }
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+  // } catch (error) {
+  //   res.status(500).json({
+  //     status: 500,
+  //     message: error.message,
+  //   });
+  // }
 };
 
 export const getContactByIdController = async (req, res) => {
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -5,11 +5,12 @@ import {
   getContactsController,
   getContactByIdController,
 } from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const contactsRouter = Router();
 
-contactsRouter.get('/', getContactsController);
+contactsRouter.get('/', ctrlWrapper(getContactsController));
 
-contactsRouter.get('/:id', getContactByIdController);
+contactsRouter.get('/:id', ctrlWrapper(getContactByIdController));
 
 export default contactsRouter;
